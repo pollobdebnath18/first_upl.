7794ceb571330fa5a61f3c238bbfdfe7c32d6168
Pollob Debnath <142177764+pollobdebnath18@users.noreply.github.com>
2023-11-09 00:10:32 +0600

    Add files via upload

---
+++ contest_Off By One.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll        long long int
+#define yes       "YES"
+#define no        "NO"
+#define nl         "\n"
+using namespace std;
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+}
+
+
+int main()
+{
+   int a,b;
+   cin>>a>>b;
+   cout<<a+b<<1<<endl;
+}
+
